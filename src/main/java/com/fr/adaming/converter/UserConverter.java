6b877cb4fb182cd374a423bc721acc7506aaa8e5
unknown <bouaouiche.sarra@gmail.com>
2020-03-10 15:28:21 +0100

    je gère ma gueule tkt

--- src/main/java/com/fr/adaming/converter/UserConverter.java
+++ src/main/java/com/fr/adaming/converter/UserConverter.java
@@ -1,5 +1,35 @@
 package com.fr.adaming.converter;
 
+import com.fr.adaming.dto.RegisterDto;
+import com.fr.adaming.entity.User;
+
 public class UserConverter {
 
+	
+	public User converterDtoToUser(RegisterDto dto) {
+		
+		User user = new User();
+		
+		
+		user.setNom(dto.getName());
+		user.setEmail(dto.getMail());
+		user.setPwd(dto.getMotDePasse());
+		
+		return user;	
+		
+	}
+	
+	public static RegisterDto convertUserToDto(User user) {
+
+		RegisterDto dto = new RegisterDto();
+
+		dto.setMail(user.getEmail());
+		dto.setMotDePasse(user.getPwd());
+		dto.setName(user.getNom());
+
+		return dto;
+
+	}
+	
+	
 }
