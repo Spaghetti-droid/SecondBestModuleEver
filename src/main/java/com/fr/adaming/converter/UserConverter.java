01f43d8c862c0cb4f1a5c1520424d90127d756c7
unknown <bouaouiche.sarra@gmail.com>
2020-03-10 15:40:47 +0100

    scilence is gold

--- src/main/java/com/fr/adaming/converter/UserConverter.java
+++ src/main/java/com/fr/adaming/converter/UserConverter.java
@@ -1,35 +1,57 @@
 package com.fr.adaming.converter;
 
+import com.fr.adaming.dto.LoginDto;
 import com.fr.adaming.dto.RegisterDto;
 import com.fr.adaming.entity.User;
 
 public class UserConverter {
 
-	
-	public User converterDtoToUser(RegisterDto dto) {
-		
+	public User regConverterDtoToUser(RegisterDto regdto) {
+
 		User user = new User();
-		
-		
-		user.setNom(dto.getName());
-		user.setEmail(dto.getMail());
-		user.setPwd(dto.getMotDePasse());
-		
-		return user;	
-		
-	}
-	
-	public static RegisterDto convertUserToDto(User user) {
 
-		RegisterDto dto = new RegisterDto();
+		user.setNom(regdto.getName());
+		user.setEmail(regdto.getMail());
+		user.setPwd(regdto.getMotDePasse());
 
-		dto.setMail(user.getEmail());
-		dto.setMotDePasse(user.getPwd());
-		dto.setName(user.getNom());
-
-		return dto;
+		return user;
 
 	}
-	
-	
+
+	public User logConverterDtoToUser(LoginDto logdto) {
+
+		User user = new User();
+
+		user.setNom(logdto.getName());
+		user.setEmail(logdto.getMail());
+		user.setPwd(logdto.getMotDePasse());
+
+		return user;
+
+	}
+
+	public static RegisterDto regConvertUserToDto(User user) {
+
+		RegisterDto regdto = new RegisterDto();
+
+		regdto.setMail(user.getEmail());
+		regdto.setMotDePasse(user.getPwd());
+		regdto.setName(user.getNom());
+
+		return regdto;
+
+	}
+
+	public static LoginDto logConvertUserToDto(User user) {
+
+		LoginDto logdto = new LoginDto();
+
+		logdto.setMail(user.getEmail());
+		logdto.setMotDePasse(user.getPwd());
+		logdto.setName(user.getNom());
+
+		return logdto;
+
+	}
+
 }
