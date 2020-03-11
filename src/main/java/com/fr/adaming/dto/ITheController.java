116fa49ebb6a812bc94fc0193f542fe598702417
unknown <gregoire_brebner@hotmail.com>
2020-03-11 15:14:45 +0100

    modif de ITheController non-sauvegardée

--- src/main/java/com/fr/adaming/dto/ITheController.java
+++ src/main/java/com/fr/adaming/dto/ITheController.java
@@ -1,5 +1,34 @@
 package com.fr.adaming.dto;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping(path = "/the")
 public interface ITheController {
 
+
+	@PostMapping
+	public ResponseEntity<?> ajouter(@RequestBody TheDto dto);
+
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> supprimerThe(@PathVariable(name = "id") int id) ;
+
+	@PutMapping
+	public ResponseEntity<?> modifier(@RequestBody FullTheDto dto) ;
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<?> get(@PathVariable(name = "id") int id) ;
+
+	@GetMapping(path = "/all")
+	public ResponseEntity<?> getAll();
+
+	
 }
